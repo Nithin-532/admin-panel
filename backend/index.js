869cde820306fbee2288e5695f553a9654479948
Nithin-532 <kallurunithin8771@gmail.com>
2024-07-19 03:18:39 +0530

    new backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
 const adminRouter = require("./routes/Admin/adminRouter");
 const userRouter = require("./routes/User/userRouter");
+const port = process.env.PORT || 3000;
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -10,4 +14,4 @@ app.use(express.json());
 app.use("/admin", adminRouter);
 app.use("/user", userRouter);
 
-app.listen(3000);
+app.listen(port);
